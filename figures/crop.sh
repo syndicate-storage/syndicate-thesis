c6bb6271c701808fd18a907b7dbe7165db94986f
Jude Nelson <judecn@gmail.com>
2017-12-01 10:57:04 -0500

    move stuff

--- figures/crop.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for psfile in $(ls *.ps); do
-    echo "$psfile"
-    pdffile="${psfile%%.ps}.pdf"
-    ps2pdf "$psfile" && pdfcrop --margins '0 0 0 0' "$pdffile" "$pdffile"
-done
-
-exit 0
